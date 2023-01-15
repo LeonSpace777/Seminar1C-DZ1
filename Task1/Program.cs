077cbca1ecd3720e0d07341a06a3df3ec0b6c177
Dmitriy Samsonov <LeonSpace777@yandex.ru>
2023-01-15 22:11:38 +0300

    Tasks files added

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a;
+int b;
+
+Console.Write("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(a);
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(b);
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(a);
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a;
+int b;
+int c;
+
+Console.Write("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(a);
+}
+
+else if (b > a && b > c)
+
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(b);
+}
+
+else if (c > a && c > b)
+
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(c);
+}
+else
+
+{
+    Console.WriteLine("Такого числа не существует");
+}
